b1080e88fefd36ee93372c006e3014a1932c834c
shime-saba <trefish22@gmail.com>
2015-12-22 15:13:33 -0800

    enormous first commit. added data, make_patch_df, patch_nlp, and 3 notebooks

---
+++ code/make_patch_df.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from collections import defaultdict
+import cPickle as pickle
+import json
+
+def get_hero_names():
+    '''
+    helper function for df_from_patch_query;
+    scrapes dota2 official site for set of hero names.
+    '''
+    r_heroes = requests.get('http://www.dota2.com/heroes/')
+    soup_heroes = BeautifulSoup(r_heroes.content, 'html.parser')
+    hero_name_set = set()
+    for column in soup_heroes.findAll('div', {'class': 'heroIcons'}):
+        for a_tag in column.findAll('a'):
+            hero_name_set.add(a_tag['href'].split('/')[-2].replace('_', ' '))
+
+    hero_name_set.remove('Natures Prophet')
+    hero_name_set.add("Nature's Prophet") # dota2.com doesn't include apostrophe
+
+    return hero_name_set
+
+def get_abi_names(filepath):
+    '''
+    builds set of ability names to use as stopwords in remove_abi_names.
+    '''
+    with open(filepath) as f:
+        abi_json = json.load(f)
+    ability_set = set(ability['localizedName'] for ability in abi_json)
+    ability_set.remove('') # discard empty string
+    return ability_set
+
+def build_df(hero_change_dict, hero_name_set, patch_name):
+    '''
+    helper function; creates DataFrame to return in df_from_patch_query
+    '''
+    heroes = []
+    change_text = []
+    for hero, text_list in hero_change_dict.iteritems():
+        heroes.append(hero)
+        change_text.append(text_list)
+
+    df = pd.DataFrame({'hero': heroes, 'text': change_text, 'patch': [patch_name]*len(heroes)})
+    changed_heroes_685 = set(df.hero.values)
+    untouched_heroes = []
+    for hero in hero_name_set:
+        if not hero in changed_heroes_685:
+            untouched_heroes.append(hero)
+
+    df_all_heroes = pd.concat([df, pd.DataFrame({'hero': untouched_heroes,
+                                                 'text': [[]]*len(untouched_heroes),
+                                                 'patch': [patch_name]*len(untouched_heroes)})])
+    df_all_heroes.sort('hero', inplace=True)
+    df_all_heroes.reset_index(inplace=True)
+    df_all_heroes.drop('index', axis=1, inplace=True)
+
+    return df_all_heroes
+
+def df_from_patch_query(page, hero_name_set, patch_name):
+    '''
+    INPUT: patch page url as string, set of acceptable hero names, patch label as string
+    OUTPUT: dataframe of hero | list of changes (text) | patch label
+    '''
+    r = requests.get('https://dota2.gamepedia.com/{0}'.format(page))
+    soup = BeautifulSoup(r.content, 'html.parser')
+
+    hero_change_dict = defaultdict(list)
+    for ul in soup.find('div', {'id': 'mw-content-text'}).find('div', recursive=False).findAll('ul', recursive=False):
+        try:
+            li = ul.find('li')
+            if li.find('a')['title'] in hero_name_set:
+                for sub_ul in li.findAll('ul', recursive=False):
+                    for sub_li in sub_ul.findAll('li', recursive=False):
+                        hero_change_dict[li.find('a')['title']].append(sub_li.text.strip())
+        except:
+            continue
+
+    df = build_df(hero_change_dict, hero_name_set, patch_name)
+    return df
+
+def remove_abi_names(changelist, abi_set=None):
+    '''
+    INPUT: one hero's list of changes, set of recognized ability names
+    OUTPUT: hero's changelist with ability names removed
+    '''
+    changes_trimmed = []
+    for i, item in enumerate(changelist):
+        changes_trimmed.append(item)
+        for abi in abi_set:
+            item = item.replace(abi, '').strip()
+            changes_trimmed[i] = item
+    return changes_trimmed
---
+++ code/patch_nlp.py
@@ -0,0 +1,134 @@
+import pandas as pd
+import numpy as np
+import cPickle as pickle
+import random
+from nltk import word_tokenize
+from nltk import ngrams
+from nltk.corpus import stopwords
+from nltk.stem.snowball import SnowballStemmer
+
+
+def add_bigrams(text_list):
+    '''
+    INPUT: list of strings
+    OUTPUT: same list with bigrams appended to each string
+    '''
+    text_list_bigrams = []
+    for text in text_list:
+        for pair in ngrams(word_tokenize(text), 2):
+            text += ' ' + ''.join(pair)
+        text_list_bigrams.append(text)
+    return text_list_bigrams
+
+def process_numbers(text):
+    '''
+    INPUT: string
+    OUTPUT: ratio between means of first two "groups" of numbers found in string
+    groups are identified as runs of numbers or percents separated by / or ->
+
+    For example, the output of "damage increased from 5/10/15/20 to 10/20/30/40"
+    would be (10+20+30+40)/(5+10+15+20) = 0.5
+    '''
+    numbers = re.findall(r'([\d\/\.%\->x]+\s.{2,13}\s[\d\/\.%\->x]+)', text)
+    if len(numbers) > 0:
+        output = numbers[0]
+        numbers = numbers[0].replace('%', '').replace('->', '/').replace('x', '').split()
+        start_vals = np.array(numbers[0].split('/')).astype(float)
+        end_vals = np.array(numbers[-1].split('/')).astype(float)
+        ratio = end_vals.mean() / start_vals.mean()
+        return round(ratio, 3)
+    else:
+        return None
+
+def add_ratios(text_list):
+    '''
+    helper function for get_info_from_df.
+    INPUT: list of strings
+    OUTPUTS:
+    (1) list of strings with "increased" or "reduced" added in before
+    first instance of the word "from" according to ratio between numbers found
+    in the string. If no numerical change is found, the string is untouched.
+    (2) list of ratios found in each string, for use in later feature
+    engineering.
+    '''
+    ratios = []
+    text_list_ratios = []
+    for text in text_list:
+        ratio = process_numbers(text)
+        ratios.append(ratio)
+        if ratio:
+            text_split = text.split('from', 1)
+            if ratio > 1:
+                if not 'increased' in text:
+                    text = text_split[0] + 'increased from' + text_split[1]
+            elif ratio < 1:
+                if not 'reduced' in text:
+                    text = text_split[0] + 'reduced from' + text_split[1]
+        text_list_ratios.append(text)
+    return text_list_ratios, ratios
+
+
+def get_info_from_df(df, labeled=False, check_ratios=True, nostops=True, snowball=True, bigrams=True):
+    '''
+    INPUT: DataFrame, boolean for presence of change_type labels,
+           several optional parameters for text processing
+    OUTPUT: text processed according to specified parameters,
+            change_type labels (if specified), list of ratios (see above)
+    '''
+    change_texts = []
+    labels = []
+    ratios = []
+    for i, row in df.iterrows():
+        for j, text in enumerate(row['text_no_abi']):
+            change_texts.append(text.lower().replace('\n', ''))
+            if labeled:
+                labels.append(row['change_type'][j])
+
+    if check_ratios:
+        change_texts, ratios = add_ratios(change_texts)
+    if nostops:
+        stops = set(stopwords.words('english'))
+        change_texts = [' '.join([word for word in text.split(' ') if word not in stops]) for text in change_texts]
+    if snowball:
+        snowball = SnowballStemmer('english')
+        change_texts = [' '.join([snowball.stem(word) for word in text.split(' ')]) for text in change_texts]
+    if bigrams:
+        change_texts = add_bigrams(change_texts)
+
+    return change_texts, labels, ratios
+
+def kFold_TFIDF_CV(text_data, labels, model, n_folds=3, use_idf=True, shuffle_seed=None, **params):
+    '''
+    INPUT: list of strings, labels, ML model to fit, additional optional parameters
+    OUTPUT: dictionary with accuracy and auc scores for the given model
+
+    the purpose of this function is to perform kfold cross-validation of TFIDF
+    data without the TFIDF matrices with test/holdout data.
+    '''
+    text_label_zip = zip(text_data, labels)
+    if shuffle_seed:
+        random.seed(shuffle_seed)
+    random.shuffle(text_label_zip)
+    text_shuffled = np.array([row[0] for row in text_label_zip])
+    labels_shuffled = np.array([row[1] for row in text_label_zip])
+    cutoffs = [i * int(len(text_data)/float(n_folds)) for i in xrange(n_folds)] + [len(text_data)]
+
+    all_acc = []
+    all_auc = []
+    for i, start_pt in enumerate(cutoffs[:-1]):
+        end_pt = cutoffs[i+1]
+        holdout_text = text_shuffled[start_pt:end_pt]
+        holdout_labels = labels_shuffled[start_pt:end_pt]
+        train_text = np.concatenate((text_shuffled[:start_pt], text_shuffled[end_pt:]))
+        train_labels = np.concatenate((labels_shuffled[:start_pt], labels_shuffled[end_pt:]))
+
+        tfidf_train = TfidfVectorizer(use_idf=use_idf)
+        tfmat_train = tfidf_train.fit_transform(train_text)
+        tfmat_test = tfidf_train.transform(holdout_text)
+
+        model.set_params(**params)
+        model.fit(tfmat_train.toarray(), train_labels)
+        all_acc.append(model.score(tfmat_test.toarray(), holdout_labels))
+        all_auc.append(roc_auc_score(holdout_labels, model.predict_proba(tfmat_test.toarray())[:, 1]))
+
+    return {'acc': np.mean(all_acc), 'auc': np.mean(all_auc)}
